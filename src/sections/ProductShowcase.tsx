a670f0e6ce65a2a87d51edfc3af5e695e3b6b909
  dimabielov <dimabielov@Noutbuk-dimabielov.local>
2024-10-12 21:21:43 +0300

    finish Landing Page

--- src/sections/ProductShowcase.tsx
+++ src/sections/ProductShowcase.tsx
@@ -1,3 +1,62 @@
+'use client'
+
+import productImage from '@/assets/product-image.png'
+import Image from 'next/image';
+import pyramidImage from '@/assets/pyramid.png';
+import tubeImage from '@/assets/tube.png';
+import { motion, useScroll, useTransform } from 'framer-motion';
+import { useRef } from 'react';
+
 export const ProductShowcase = () => {
-  return null;
+
+  const sectionRef = useRef(null);
+
+  const { scrollYProgress } = useScroll({
+    target: sectionRef,
+    offset: ["start end", "end start"],
+  })
+
+  const translateY = useTransform(scrollYProgress, [0, 1], [150, -150]);
+
+
+
+  return (
+    <section ref={sectionRef} className="bg-gradient-to-b from-[#FFF] to-[#D2DCFF] py-24 overflow-x-clip">
+      <div className="container">
+        <div className="section-heading">
+          <div className="flex justify-center">
+            <div className="tag"> Boost your productivity</div>
+          </div>
+          <h2 className="section-title mt-5">A more effective way to track progress</h2>
+          <p className="section-description mt-5">Celebrate the joy of accomplishment with an app designed to track your progress and motivate your efforts.</p>
+        </div>
+        <div className="relative">
+            <Image 
+            src={productImage} 
+            alt="product image" 
+            className="mt-10"/>
+            <motion.img
+            src={pyramidImage.src} 
+            alt="pyramid image" 
+            height={262} 
+            width={262} 
+            className="hidden md:block absolute -right-36 -top-32"
+            style={{
+              translateY: translateY
+            }}
+            />
+            <motion.img
+            src={tubeImage.src} 
+            alt="tube image" 
+            height={240}  
+            width={248}
+            className="hidden md:block absolute bottom-24 -left-36"
+            style={{
+              translateY: translateY
+            }}
+            />
+          </div>
+      </div>
+    </section>
+  );
 };
